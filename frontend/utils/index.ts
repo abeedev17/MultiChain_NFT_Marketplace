6b8f831560482b292484c5eb047901847ab1ad4a
abeedev17 <as.asuperstar05@gmail.com>
2022-08-21 23:19:06 +0500

    added wallet signer

---
+++ frontend/utils/index.ts
@@ -0,0 +1,32 @@
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal'
+
+export const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
+
+export const getSigner = async () => {
+    try {
+      const web3Modal = new Web3Modal();
+      const connection = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      return signer;
+    } catch (err) {
+      console.log("Error in openWeb3Modal", err);
+    }
+  };
