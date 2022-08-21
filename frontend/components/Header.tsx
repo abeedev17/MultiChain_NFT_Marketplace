10b5aec0e873b2f8f37d7f1ba7e26421f2959055
abeedev17 <as.asuperstar05@gmail.com>
2022-08-21 18:01:07 +0500

    added next ui theme

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -13,36 +13,10 @@ import Link from "next/link";
 import "sf-font";
 import Logo from "../public/assets/logo.png";
 
-const theme = createTheme({
-  type: "dark",
-  theme: {
-    colors: {
-      primaryLight: "$blue200",
-      primaryLightHover: "$blue300",
-      primaryLightActive: "$blue400",
-      primaryLightContrast: "$blue600",
-      primary: "$purple500",
-      primaryBorder: "$blue500",
-      primaryBorderHover: "$blue600",
-      primarySolidHover: "$blue700",
-      primarySolidContrast: "$white",
-      primaryShadow: "$white500",
-      transparent: "#00000000",
 
-      gradient:
-        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple300 90%)",
-      link: "#5E1DAD",
 
-      myColor: "#00000030",
-    },
-    space: {},
-    fonts: {
-      fontFamily: "SF Pro Display",
-    },
-  },
-});
-
-const Header = () => {
+const Header = (props:any) => {
+  console.log("props", props)
   return (
     <div style={{ backgroundColor: "black" }}>
       <Container xl>
--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,17 +1,47 @@
 import { Fragment, ReactNode } from "react";
-import { NextUIProvider } from "@nextui-org/react";
 import "sf-font";
 import Header from "./Header";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
 
 interface ILayout {
   children: ReactNode;
 }
 
+const theme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {
+      primaryLight: "$blue200",
+      primaryLightHover: "$blue300",
+      primaryLightActive: "$blue400",
+      primaryLightContrast: "$blue600",
+      primary: "$purple500",
+      primaryBorder: "$blue500",
+      primaryBorderHover: "$blue600",
+      primarySolidHover: "$blue700",
+      primarySolidContrast: "$white",
+      primaryShadow: "$white500",
+      transparent: "#00000000",
+      inputBorder:'2px solid #5e1dad',
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple300 90%)",
+      link: "#5e1dad",
+
+      myColor: "#00000030",
+    },
+
+    space: {},
+    fonts: {
+      fontFamily: "SF Pro Display",
+    },
+  },
+});
+
 const Layout = ({ children }: ILayout) => {
   return (
     <Fragment>
       <Header />
-      <NextUIProvider>{children}</NextUIProvider>
+      <NextUIProvider theme={theme}>{children}</NextUIProvider>
     </Fragment>
   );
 };
