d205fd4087753111cb0a932710abfca794282ec7
abeedev17 <as.asuperstar05@gmail.com>
2022-08-27 03:48:54 +0500

    Update configuration.ts

--- frontend/utils/configuration/configuration.ts
+++ frontend/utils/configuration/configuration.ts
@@ -1,32 +1,26 @@
 import SimpleCrypto from "simple-crypto-js";
-import { create } from "ipfs-http-client";
 
 const cipherKey = "&E)H@MbQeThWmZq4t7w!z%C*F-JaNdRf";
 const ethraw = "xyz";
 const hhraw =
-  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  "c40656fc764ad0dadaef75308e3619e0c7c6812923f8d436fdcc630513cd42d3";
 
 export const simpleCrypto = new SimpleCrypto(cipherKey);
 export const cipherEth = simpleCrypto.encrypt(ethraw);
 export const cipherHH = simpleCrypto.encrypt(hhraw);
 
-export const hh_Resell_Contract_Address =
-  "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
-export const hh_NFT_Contract_Address =
-  "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
+export let hh_Resell_Contract_Address =
+  "0xC06bbF6F52E6FB2a699868e90BcC140c691DAff3";
+
+export let hh_NFT_Contract_Address =
+  "0xAfE4F995C1bCf3017fB84303D63Bc7677c0FA91e";
 
 export const hh_NFT_Create_Address =
-  "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
+  "0xff05aF051AAa442c8c0d8B768EdbB4f2d32A77A4";
 
 export const hh_Resell_Custom_NFT_Address =
-  "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
-const hhrpc = "http://localhost:8545";
+  "0x68f10D9b6292a031e23c560408e8C7556919a350";
 
-let projectId = "2Do1aq9LEnyxqFwrmPtm77e7nZZ";
-let projectSecret = "795fa7af4645b27c69a6e2fe4a152d75";
-export let auth =
-  "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
-// ifps config
-export const client = create("https://ipfs.infura.io:5001/api/v0" as any);
+const hhrpc = "https://rpc.ankr.com/eth_ropsten";
 
 export var mainnet = hhrpc;
